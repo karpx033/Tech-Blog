5f63a2dc65c1c3b4fbf1a345ea0f82b4e391b80a
Louis <karpx033@umn.edu>
2022-02-05 15:02:45 -0600

    added in all the post routes

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,1 +1,23 @@
+const router = require('express').Router();
+const { Comment, Post, User } = require('../models');
+const withAuth = require('../utils/auth');
 
+// Use withAuth middleware to prevent access to route
+router.get('/', withAuth, async (req, res) => {
+    try {
+      // Find the logged in user based on the session ID
+      const userData = await User.findByPk(req.session.user_id, {
+        attributes: { exclude: ['password'] },
+        include: [{ model: Post }],
+      });
+  
+      const user = userData.get({ plain: true });
+  
+      res.render('all-posts-admin', {
+        ...user,
+        logged_in: true
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
